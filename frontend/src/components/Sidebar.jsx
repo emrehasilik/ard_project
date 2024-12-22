0c86738372fd9d37ee72d53fa15d7b2a96aad711
MikiJRs <emerttd@gmail.com>
2024-12-22 21:14:47 +0300

    desinge changes

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,45 +1,34 @@
-// src/components/Sidebar.jsx
-
 import React from "react";
 import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
-    <div className="h-screen w-60 bg-[#002855] text-white shadow-md flex flex-col justify-between">
+    <div className="h-screen w-64 bg-gradient-to-b from-[#0A2637] to-[#144272] text-white shadow-xl flex flex-col justify-between">
       {/* Üst Logo ve Menü Alanı */}
       <div>
-     <div className="p-4 text-lg font-bold flex items-center border-b border-[#D4AF37]">
-  <img
-    src="/baro_logo.jpg"
-    alt="Logo"
-    className="w-24 h-24 object-cover border-2 border-[#D4AF37] rounded-full"
-  />
-  <span className="ml-3 text-white">BARO</span>
-</div>
+        <div className="p-5 text-xl font-bold flex items-center border-b border-[#C9A23E]">
+          <img
+            src="/baro_logo.jpg"
+            alt="Logo"
+            className="w-20 h-20 object-cover border-2 border-[#C9A23E] rounded-full"
+          />
+          <span className="ml-4 text-[#C9A23E]">BARO</span>
+        </div>
 
-
-        <ul className="mt-4 space-y-2">
-          <li className="py-2 px-4 hover:bg-[#D4AF37] hover:text-black cursor-pointer">
-            <Link
-              to="/basvurular"
-              className="block w-full h-full"
-            >
-              Başvurular
-            </Link>
-          </li>
-          <li className="py-2 px-4 hover:bg-[#D4AF37] hover:text-black cursor-pointer">
-            <Link
-              to="/avukatlar"
-              className="block w-full h-full"
-            >
+        <ul className="mt-6 space-y-3">
+          <li className="py-2 px-5 hover:bg-[#C9A23E] hover:text-black cursor-pointer rounded-md">
+            <Link to="/avukatlar" className="block w-full h-full">
               Avukatlar
             </Link>
           </li>
-          <li className="py-2 px-4 hover:bg-[#D4AF37] hover:text-black cursor-pointer">
-            <Link
-              to="/davalar"
-              className="block w-full h-full"
-            >
+          <li className="py-2 px-5 hover:bg-[#C9A23E] hover:text-black cursor-pointer rounded-md">
+            <Link to="/basvurular" className="block w-full h-full">
+              Başvurular
+            </Link>
+          </li>
+
+          <li className="py-2 px-5 hover:bg-[#C9A23E] hover:text-black cursor-pointer rounded-md">
+            <Link to="/davalar" className="block w-full h-full">
               Davalar
             </Link>
           </li>
@@ -47,9 +36,9 @@ const Sidebar = () => {
       </div>
 
       {/* Alt Menü */}
-      <div className="p-4 border-t border-[#D4AF37]">
-        <button className="w-full bg-[#D4AF37] text-black py-2 rounded hover:bg-[#B89B2F]">
-          Logout
+      <div className="p-5 border-t border-[#C9A23E]">
+        <button className="w-full bg-[#C9A23E] text-black py-2 rounded-md hover:bg-[#A78936]">
+          Çıkış Yap
         </button>
       </div>
     </div>
